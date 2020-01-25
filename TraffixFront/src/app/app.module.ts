d64d82b3229c4427e2ba71913516434680f61fe8
Oussama <saoudio@mcmaster.ca>
2020-01-25 14:16:20 -0500

    Added essential components

--- TraffixFront/src/app/app.module.ts
+++ TraffixFront/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapPresentComponent } from './map-present/map-present.component';
+import { GraphsComponent } from './graphs/graphs.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapPresentComponent,
+    GraphsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
