8eeea93704ff5ada8caf7b5a428344ef5682a7bd
Oussama <saoudio@mcmaster.ca>
2020-01-25 14:36:46 -0500

    Removed added components

--- TraffixFront/src/app/app.module.ts
+++ TraffixFront/src/app/app.module.ts
@@ -3,16 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MapPresentComponent } from './map-present/map-present.component';
-import { GraphsComponent } from './graphs/graphs.component';
-import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MapPresentComponent,
-    GraphsComponent,
-    LoginComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
