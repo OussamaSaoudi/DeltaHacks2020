c77dfad9387e8ee04fc351a45be721f2959384f8
Julian Woo <julian.wooville@gmail.com>
2020-01-25 15:32:05 -0500

    Finalized barebones Django setup

--- backend/deltahacks2020/deltahacks_app/views.py
+++ backend/deltahacks2020/deltahacks_app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.http import HttpResponse, JsonResponse
 
-# Create your views here.
+from new_app.models import ExampleModel
+from new_app.serializers import ExampleModelSerializer
+
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
+def get_data(request):
+	data = ExampleModel.objects.all()
+	if request.method == 'GET':
+		serializer = ExampleModelSerializer(data, many=True)
+		return JsonResponse(serializer.data, safe=False)
