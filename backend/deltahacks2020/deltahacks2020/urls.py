90b0de44e738ea932208df71441027c040c49857
Julian Woo <julian.wooville@gmail.com>
2020-01-25 22:40:20 -0500

    Complete overhaul of backend

--- backend/deltahacks2020/deltahacks2020/urls.py
+++ backend/deltahacks2020/deltahacks2020/urls.py
@@ -1,7 +1,7 @@
-"""{{ project_name }} URL Configuration
+"""deltahacks2020 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,21 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
-
-1
-2
-3
-4
-5
-6
-7
-8
-from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path, include
- 
+from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from api import views
+
+router = routers.DefaultRouter()
+router.register(r'movies', views.MovieViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^', include('data.urls'))
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- backend/deltahacks2020/deltahacks_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DeltahacksAppConfig(AppConfig):
-    name = 'deltahacks_app'
