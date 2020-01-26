dcf46e4f44b5e95252e539b68c8bc64955efdde9
Julian Woo <julian.wooville@gmail.com>
2020-01-26 00:21:01 -0500

    Harmonized front & back end; working movie site

--- backend/deltahacks2020/api/views.py
+++ backend/deltahacks2020/api/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from .serializers import MovieSerializer
+from .serializers import MovieSerializer, MovieMiniSerializer
 from .models import Movie
+from rest_framework.response import Response
 
 class MovieViewSet(viewsets.ModelViewSet):
     """
@@ -9,3 +10,8 @@ class MovieViewSet(viewsets.ModelViewSet):
     """
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+
+    def list(self, request, *args, **kwargs):
+        movies = Movie.objects.all()
+        serializer = MovieMiniSerializer(movies, many=True)
+        return Response(serializer.data)
--- backend/deltahacks2020/deltahacks2020/settings.py
+++ backend/deltahacks2020/deltahacks2020/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 CORS_ORIGIN_WHITELIST = [
-    "http://127.0.0.1:9000"
+    "http://localhost:4200"
 ]
 
 
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'api',
     'corsheaders',
 ]
